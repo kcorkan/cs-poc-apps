9f33a3f537c58d0bc72de0de5f63d349ad574f6d
kcorkan <kcorkan@rallydev.com>
2016-04-11 14:35:24 -0600

    updates to row expander

--- planning-app-no-children/App.js
+++ planning-app-no-children/App.js
@@ -33,25 +33,115 @@ Ext.define('CustomApp', {
 
         this._buildGrid();
     },
+    _updateModels: function(store, records){
+        Ext.Array.each(records, function(r){
+            r.set('homeDemand', 4);
+            r.set('visitorDemand',4);
+            r.set('totalDemand', 8);
+            r.set('buildingBlocks',[{
+                team: 'home',
+                pin:  "Home PIN",
+                name: 'BB1',
+                amount: 4
+            },{
+                team: 'visiting',
+                pin:  'PIN1',
+                name: 'BB2',
+                amount: 3
+            },{
+                team: 'visiting',
+                pin:  'PIN1',
+                name: 'BB3',
+                amount: 1
+            }]);
+        })
+    },
     _buildGrid: function(){
-        this.add({
+        var grid = this.add({
             xtype: 'rallygrid',
+            itemId: 'planning-grid',
             storeConfig: {
                 model: this.parentTypePath,
                 fetch: this.parentFetch,
-                autoLoad: true
+                autoLoad: true,
+                listeners: {
+                    load: this._updateModels,
+                    scope: this
+                },
             },
+            margin: 25,
             columnCfgs: this._getColumnCfgs(),
             showRowActionsColumn: true,
             plugins: [{
                 ptype: 'rowexpander',
-                rowBodyTpl: new Ext.XTemplate('<p>{Name}</p>{[this.getBuildingBlocks(values)]}',{
-                    getBuildingBlocks: function(values){
-                        return Ext.String.format('<br/><span class="building-blocks">Building Blocks go here...</span>');
-                    }
-                })
+                rowBodyTpl: '<div id="planning-{FormattedID}"> </div>'
             }]
         });
+        grid.getView().on('expandbody', this._expandRowBody, this);
+    },
+
+    _expandRowBody2: function(rowNode, record, expandRow, options){
+
+    },
+    _expandRowBody: function(rowNode, record, expandRow, options){
+        var ct = Ext.get(expandRow.querySelector('#planning-' + record.get('FormattedID'))),
+            data = record.get('buildingBlocks');
+
+        console.log('_expandRowBody', rowNode, record, expandRow, options, ct);
+        ct.setHeight(200);
+
+        var grid = ct.down('#planning-row-' + record.get('FormattedID'));
+        if (grid){
+            console.log('grid found', grid);
+            grid.destroy();
+        }
+
+        Ext.create('Rally.ui.grid.Grid',{
+            itemId: 'planning-row-' + record.get('FormattedID'),
+            store: Ext.create('Rally.data.custom.Store',{
+                data: data,
+                fields: ['team','pin','name','amount'],
+                groupField: 'team',
+                groupDir: 'ASC',
+                getGroupString: function(record) {
+                    var team = record.get('team');
+                    if (team === "home"){
+                        return 'Home Team'
+                    }
+                    return "Visiting Teams";
+                }
+            }),
+            pageSize: data.length,
+            showPagingToolbar: false,
+            hideHeaders: true,
+            features: [{
+                ftype: 'groupingsummary',
+                groupHeaderTpl: '{name} ({rows.length})',
+                startCollapsed: false
+            }],
+            columnCfgs: [{
+                dataIndex: 'pin',
+                text: 'pin',
+                flex: 1
+            },{
+                dataIndex: 'name',
+                text: 'name',
+                flex: 1
+            },{
+                dataIndex: 'amount',
+                text: 'amount',
+                editor: {
+                    xtype: 'rallynumberfield',
+                    listeners: {
+                        change: function(x,y,z){
+                            console.log('nb', x,y,z, record);
+                        }
+                    }
+                }
+            }],
+            renderTo: ct
+        });
+
     },
     _getColumnCfgs: function(){
         return [{
@@ -60,6 +150,22 @@ Ext.define('CustomApp', {
             dataIndex: 'Name'
         },{
             dataIndex: 'Project'
+        },{
+            xtype: 'templatecolumn',
+            text: 'Total Demand',
+            tpl: '{homeDemand} + {visitorDemand} = {totalDemand}'
+        },{
+            xtype: 'templatecolumn',
+            text: 'Home Demand',
+            tpl: '{homeDemand}'
+        },{
+            xtype: 'templatecolumn',
+            text: 'Visitor Demand',
+            tpl: '{visitorDemand}'
+        },{
+            dataIndex: 'PlannedStartDate'
+        },{
+            dataIndex: 'PlannedEndDate'
         }];
     }
 });
